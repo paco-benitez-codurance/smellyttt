357084d2f6305599df22c356a0e27ba037b58096
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-05-13 15:49:38 +0200

    Duplicated Code

--- app/src/main/java/Game.java
+++ app/src/main/java/Game.java
@@ -28,9 +28,7 @@ public class Game {
         //if the positions in first row are taken
         if (isRowFree(0)) {
             //if first row is full with same symbol
-            if (_board.TileAt(0, 0).Symbol ==
-                    _board.TileAt(0, 1).Symbol &&
-                    _board.TileAt(0, 2).Symbol == _board.TileAt(0, 1).Symbol) {
+            if (sameSymbolInRow(0)) {
                 return _board.TileAt(0, 0).Symbol;
             }
         }
@@ -38,10 +36,7 @@ public class Game {
         //if the positions in first row are taken
         if (isRowFree(1)) {
             //if middle row is full with same symbol
-            if (_board.TileAt(1, 0).Symbol ==
-                    _board.TileAt(1, 1).Symbol &&
-                    _board.TileAt(1, 2).Symbol ==
-                            _board.TileAt(1, 1).Symbol) {
+            if (sameSymbolInRow(1)) {
                 return _board.TileAt(1, 0).Symbol;
             }
         }
@@ -49,15 +44,17 @@ public class Game {
         //if the positions in first row are taken
         if (isRowFree(2)) {
             //if middle row is full with same symbol
-            if (_board.TileAt(2, 0).Symbol ==
-                    _board.TileAt(2, 1).Symbol &&
-                    _board.TileAt(2, 2).Symbol ==
-                            _board.TileAt(2, 1).Symbol) {
+            if (sameSymbolInRow(2)) {
                 return _board.TileAt(2, 0).Symbol;
             }
         }
 
         return ' ';
+    }
+
+    private boolean sameSymbolInRow(int x) {
+        return _board.TileAt(x, 0).Symbol == _board.TileAt(x, 1).Symbol &&
+          _board.TileAt(x, 2).Symbol == _board.TileAt(x, 1).Symbol;
     }
 
     private boolean isRowFree(int x) {
